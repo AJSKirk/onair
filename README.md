7be0a29a76913a6f4c415d5b9376ddf5fa6576b6
Andrew Kirk <ajskirk@gmail.com>
2021-01-28 19:57:50 +0000

    Initial commit with Python GCal API working

---
+++ README.md
@@ -0,0 +1,5 @@
+# On Air Sign
+
+I'm trying to build an ESP8266 powered LED light in an old school radio recording booth "On Air" sign. The idea is to hook it up to various APIs to automatically indicate if I'm on a call, so others in the house know not to barge into the room.
+
+`arduino/` code contains Arduino C code for running on the embedded devices, `python/` contains code for prototyping API creds and calls.
---
+++ python/google_calendar.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from datetime import datetime
+import click
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def get_creds():
+    """Authorisation boilerplate from Google's quickstart template"""
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
+
+def get_busies(creds, time_min: datetime, time_max: datetime, email: str, time_zone: str = "UTC"):
+    service = build('calendar', 'v3', credentials=creds)
+
+    # I think ISOFormat works, else its YYYY-MM-DDTHH:MM:SS.ppZ
+    body = {"timeMin": time_min.isoformat() + 'Z', "timeMax": time_max.isoformat() + 'Z', "timeZone": time_zone,
+            "items": [{"id": email}]}
+
+    results = service.freebusy().query(body=body).execute()
+    busies = results['calendars'][email]['busy']
+    return busies
+
+
+@click.command()
+@click.argument('email')
+def main(email):
+    """Retrieves today's busy times for user email supplied as command line argument"""
+    creds = get_creds()
+    start = datetime.now().replace(hour=0, minute=0, second=0)
+    end = datetime.now().replace(hour=23, minute=59, second=59)
+    busies = get_busies(creds, start, end, email)
+    print(busies)
+
+
+if __name__ == '__main__':
+    main()
